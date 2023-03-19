6c13bc6f37828b1311cb2678bc5673f01fe17a4e
lucasact4 <lucasact4@outlook.com>
2023-03-19 12:59:33 -0300

    Curso Ruby Adicionado

---
+++ Ruby/Ruby/hello.rb
@@ -0,0 +1,15 @@
+puts "Hello World!"
+p 'How are you?'
+
+var = 10
+var.class
+
+print "nome: "
+nome = gets.chomp
+print "nome: #{nome}"
+
+puts "\n"
+
+print "número: "
+número = gets.chomp.to_i
+print "número: #{número}"
