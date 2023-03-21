86f21ea63600b1228a16f47bfa312824c74541f8
lucasact4 <lucasact4@outlook.com>
2023-03-21 14:38:13 -0300

    Create 2_projeto_vue
    Criado o projeto para estudos em vue.js

---
+++ Vue.js/cli/2_projeto_vue/src/components/Info.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+    <p v-else>Estou em busca de novas oportunidades.</p>
+    <p>Utilizo as seguintes tecnologias:</p>
+    <ul>
+      <li>JavaScript</li>
+      <li>PHP</li>
+      <li>Python</li>
+    </ul>
+    <div>
+
+      <button @click="showEmail">{{ textoBotao }}</button>
+    </div>
+    <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+    <p class="teste">Para acessar meu portfólio basta <a v-bind:href="meu_link" target="_blank">clicar aqui</a></p>
+    <Picture />
+  </div>
+</template>
+<script>
+  import Picture from './Picture.vue'
+  export default {
+    name: 'Info',
+    components: {
+      Picture
+    },
+    created() {
+      this.showEmail()
+    },
+    data() {
+      return {
+        esta_trabalhando: true,
+        mostrar_email: true,
+        email: 'funcraftact4@outlook.com',
+        meu_link: 'https://google.com',
+        textoBotao: 'Mostrar e-mail'
+      }
+    },
+    methods: {
+      showEmail() {
+        this.mostrar_email = !this.mostrar_email
+        this.stateEmail()
+      },
+      stateEmail() {
+        if(!this.mostrar_email) {
+          this.textoBotao = 'Mostrar e-mail'
+        } else {
+          this.textoBotao = 'Esconder e-mail'
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  .paragrafo-pai {
+    color: red;
+  }
+</style>
