643bd6473e442f732aefef1c28a290799fa1def5
lucasact4 <lucasact4@outlook.com>
2023-03-23 23:51:40 -0300

    Att

--- Vue.js/cli/3_make_your_burger/src/components/BurgerForm.vue
+++ Vue.js/cli/3_make_your_burger/src/components/BurgerForm.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <p>Componente de Mensagem</p>
+    <Message :msg="msg" v-show="msg" />
     <div>
-      <form id="burger-form">
+      <form id="burger-form" @submit="createBurger">
         <div class="input-container">
           <label for="nome">Nome do cliente:</label>
           <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite o seu nome">
@@ -37,6 +37,7 @@
 </template>
 
 <script>
+  import Message from './Message.vue'
   export default {
     name: 'BurgerForm',
     data() {
@@ -48,7 +49,6 @@
         pao: null,
         carne: null,
         opcionais: [],
-        status: "Solicitado",
         msg: null
       }
     },
@@ -62,10 +62,45 @@
         this.carnes = data.carnes
         this.opcionaisdata = data.opcionais
 
+      },
+      async createBurger(e) {
+
+        e.preventDefault()
+        
+        const data = {
+          nome: this.nome,
+          carne: this.carne,
+          pao: this.pao,
+          opcionais: Array.from(this.opcionais),
+          status: "Solicitado"
+        }
+
+        const dataJson = JSON.stringify(data)
+
+        const req = await fetch("http://localhost:3000/burgers", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: dataJson
+        })
+
+        const res = await req.json()
+
+        this.msg = `Pedido Nº ${res.id} realizado com sucesso`
+
+        setTimeout(() => this.msg = "", 10000)
+
+        this.nome = ""
+        this.carne = ""
+        this.pao = ""
+        this.opcionais = ""
+
       }
     },
     mounted() {
       this.getIngredientes()
+    },
+    components: {
+      Message
     }
   }
 </script>
