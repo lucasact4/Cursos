6d36c55d73da63d4dda827765e5b67659535276e
lucasact4 <lucasact4@outlook.com>
2023-03-30 02:18:28 -0300

    Bootstrap

---
+++ Rails/myrecipes/app/javascript/application.js
@@ -0,0 +1,13 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
+//= require jquery
+//= require jquery-ujs
+//= require popper
+//= require bootstrap-sprockets
+//= require rails-ujs
+//= require turbolinks
+//= require activestorage
+//= require_tree .
+//= require bootstrap
