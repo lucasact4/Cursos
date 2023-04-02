394a83db4c50b63b12cfec3f001bc4560744eedc
lucasact4 <lucasact4@outlook.com>
2023-04-02 00:22:00 -0300

    Add flash message partial

--- Rails/myrecipes/app/javascript/application.js
+++ Rails/myrecipes/app/javascript/application.js
@@ -1,11 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
-//= require jquery
+//= require jquery3
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require turbolinks
-//= require_tree .
-
 
 import "@hotwired/turbo-rails"
 import "controllers"
