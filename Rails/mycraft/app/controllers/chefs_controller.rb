4a637b209581e49e496217377180c3445ed027ed
lucasact4 <lucasact4@outlook.com>
2023-04-10 20:47:13 -0300

    Design de todos os botôes atualizados

--- Rails/mycraft/app/controllers/chefs_controller.rb
+++ Rails/mycraft/app/controllers/chefs_controller.rb
@@ -52,7 +52,7 @@ class ChefsController < ApplicationController
   def destroy
     if !@chef.admin?
       @chef.destroy
-      flash[:danger] = "Player e todas as postagens associadas foram deletadas!"
+      flash[:danger] = "Jogador e todas as postagens associadas foram deletadas!"
       redirect_to chefs_path
     end
   end
